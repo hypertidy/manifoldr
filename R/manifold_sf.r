4b22a0c87b99cf6d671b5736a3a7100e65528cff
Michael Sumner <mdsumner@gmail.com>
2016-11-13 19:57:07 +1100

    sf mostly working now

--- R/manifold_sf.r
+++ R/manifold_sf.r
@@ -11,60 +11,69 @@
 #' dwg  <- Drawing(mapfile, "Drawing")
 #' ## only lines
 #' dwg_sub <- Drawing(mapfile, WHERE = "WHERE [Type (I)] = 2")
+#' dwg_sub
 Drawing <- function(mapfile, dwgname, ...) {
-  sfmfd(mapfile, dwgname, ...)
+  con <- odbcConnectManifold(mapfile)
+  mfd_read_db(con, dwgname, ...)
 }
 
 query_mfd <- function(dsn, query) {
   on.exit(.cleanup(con))
-  # if (!checkAvailability()) {stop("Manifold is not installed, but is required for connection to project files.")}
   con <- odbcConnectManifold(dsn)
   RODBC::sqlQuery(con, query)
 }
 
+.choose_table <- function(con, verbose) {
+  #geometry_columns <- RODBC::sqlQuery(con, "SELECT * FROM [geometry_columns]")
+  #available_OGC_tables <- unique(geometry_columns$F_TABLE_NAME)
+  available_tables <- sqlTables(con)
+  avt <- available_tables[available_tables[["TABLE_TYPE"]] == "TABLE", ]
+  if (nrow(avt) > 0) {
+    ##table <- available_OGC_tables[1L] 
+    table <- avt$TABLE_NAME[1]
+    if (verbose) print(sprintf("choosing first table [%s] from: ", table))
+    if (verbose) print(paste(avt$TABLE_NAME, collapse = ","))
+    ## maybe list the actual tables as well ...
+  } else {
+    stop("no OGC-compliant tables available")
+  }
+  table
+}
 #' @export
 #' @importFrom sf st_as_sfc st_as_sf
 #' @importFrom RODBC sqlTables
 #' @importFrom tibble as_tibble
-sfmfd <- function(dsn, table, sf_geom_name = "geom", crs = NULL, WHERE = "", quiet = FALSE) {
-   atts <- "*"
+mfd_read_db <- function(con = NULL, table, 
+                        query = "", 
+                        geom_column = NULL,
+                        WHERE = "", ..., quiet = TRUE) {
    verbose = !quiet
-   on.exit(.cleanup(con))
-    #mc <- mapcontents(dsn)
-   con <- odbcConnectManifold(dsn)
+   
    if (missing(table)) {
-     available_tables <- RODBC::sqlTables(con)
-     avt <- available_tables[available_tables[["TABLE_TYPE"]] == "TABLE", ]
-     if (nrow(avt) > 0) {
-       table <- avt$TABLE_NAME[1]
-       if (verbose) print(sprintf("choosing table [%s] from: ", table))
-       if (verbose) print(paste(avt$TABLE_NAME, collapse = ","))
-     } else {
-       stop("no tables available")
-     }
+     table <- .choose_table(con, verbose)
    }
-    attributes <- manifoldr:::columnames(con, table)
-    #mc$columns$colnames[mc$columns$tableID == mc$tables$ID[which(mc$tables$TABLE_NAME == table)]]
-    #close(con)
-    attributes <- 
-      paste0("[", attributes[-grep(" \\(I\\)", attributes)], "]")
-    atts <- sprintf("%s, CGeomWKB(Geom(ID)) AS [%s]", paste(attributes, collapse = ","), sf_geom_name)
-    crswkt <- manifoldr:::manifoldCRS(con, table)
-    if (is.null(crs)) {
-      crs <- manifoldr:::wktCRS2proj4(crswkt)
-    }
+   available_colnames <- manifoldr:::columnames(con, table)
+  crswkt <- manifoldr:::manifoldCRS(con, table)
+  if (verbose) print(crswkt)
+    crs <- manifoldr:::wktCRS2proj4(crswkt)
+    if (verbose) print(crs)
+   if (is.null(geom_column)) geom_column <- "geom"
+    ## here we might use the OGC names, but it complicates things a bit trying to balance everything
+    ## geometry_columns is pretty useless getting the CRS and the actual table names as they appear in Manifold
+    ## so forget it
+    available_colnames <- 
+      paste0("[", available_colnames[-grep(" \\(I\\)", available_colnames)], "]")
+    atts <- sprintf("%s, CGeomWKB(Geom(ID)) AS [%s]", paste(available_colnames, collapse = ","), geom_column)
+    
+    
     query <- sprintf("SELECT %s FROM [%s] %s", atts, table,  WHERE)
+    #if (dropNULL) {
+   query <- sprintf("SELECT * FROM (%s) WHERE [geom] IS NOT NULL", query)
+  #  }
     if (verbose) print(query)
-  x <-  manifoldr:::query_mfd(dsn, query)
-  empty <- which(unlist(lapply(x[[sf_geom_name]], length)) < 1)
-  if (length(empty) > 0) {
-   # x[[sf_geom_name]][empty] <- replicate(length(empty), NULL)
-    ## drop it
-    if (verbose) print(sprintf("dropping %i empty features", length(empty)))
-    x <- x[-empty, ]
-    if (nrow(x) < 1L) stop("all geometries are empty")
-  }
+  x <-  RODBC::sqlQuery(con, query)
+
   x <- tibble::as_tibble(x)
-  x[[sf_geom_name]] <- sf::st_as_sfc(structure(x[[sf_geom_name]], class = "WKB"), EWKB = FALSE)
+  x[["geom"]] <- sf::st_as_sfc(structure(x[["geom"]], class = "WKB"), EWKB = FALSE)
   st_as_sf(x)
 }
